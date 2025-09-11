b62f4089dad8cc29f7cf055447984cf863c12f1c
Michael Ekoka <michael@sundry.ca>
2025-09-11 14:02:07 -0500

    Update kadane drill

--- ds-and-a/202508/kadane.py
+++ ds-and-a/202508/kadane.py
@@ -1,12 +1,15 @@
-def maxsum(array):
-    n = len(array)
-    if n==0: return
-
-    running_sum = array[0]
+def kadane(array):
+    running_sum = -float('inf')
     maxsum = running_sum
-    i = 1
-    while i < n:
-        running_sum = max(running_sum + array[i], array[i])
+    for i in range(len(array)):
+        running_sum = maxsum(running_sum + array[i], array[i])
         maxsum = max(maxsum, running_sum)
-        i += 1
+    return maxsum
 
+def kadane(array):
+    running_sum = -float('inf')
+    maxsum = running_sum
+    for v in array:
+        running_sum = max(running_sum + v, v)
+        maxsum = max(maxsum, running_sum)
+    return maxsum
